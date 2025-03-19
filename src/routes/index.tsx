5e357a9a90b076ce3273833351074c676225cb04
Adrian Zinko <zinko.adrian00@gmail.com>
2025-03-19 23:02:44 +0100

    register service worker on app init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,17 @@
 import { getCurrentUser } from "@/lib/firebase/firebase";
+import { registerServiceWorker } from "@/lib/service-worker";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   loader: async () => {
+    const sw = await registerServiceWorker();
+
+    if (sw) {
+      console.log("Service worker registered");
+    } else {
+      console.log("Service worker not registered");
+    }
+
     const currentUser = await getCurrentUser();
 
     if (!currentUser) {
